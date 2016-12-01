221d4c84d293f32b64d25a96ea57bb87e32bc3eb
Naomi O'Sullivan <naomiosullivan@Naomis-MacBook-Air.local>
2016-12-02 00:07:38 +1100

    created js cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,5 @@
 console.log("JS file is connected to HTML! Woo!");
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
@@ -21,6 +21,17 @@ if(cardThree === cardFour){
 }
 else {
 	alert("Sorry, try again!");
+}*/
+
+var board = document.getElementById('game-board');
+
+var createCards = function() {
+	
+	for(var i = 0; i<4; i++) {
+		var gameCard = document.createElement('div');
+		gameCard.className = 'card';
+		board.appendChild(gameCard)
+	}
+		
 }
-
-
+createCards();
